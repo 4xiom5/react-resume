8daf1f9a215ca6922007b7e1a7b19c8dcffd9dbc
Brice Theurillat <brice.theurillat@protonmail.ch>
2022-08-18 15:47:16 +0200

    Add basic layout

--- src/Page.js
+++ src/Page.js
@@ -1,9 +1,13 @@
-import './Page.css';
 
 function Page() {
   return (
-    <div className='page'>
+    <div className="w-a4 h-a4 mx-auto my-4 shadow-2xl">
+      <div className="w-1/3 h-full bg-slate-100">
 
+      </div>
+      <div className="w-full h-full">
+
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        a4: '210mm',
+      },
+      height: {
+        a4: '297mm',
+      },
+    },
   },
   plugins: [],
 }
