a89f46a1562cf7de24af0705a0debf9eae4a8bd0
Brice Theurillat <brice.theurillat@protonmail.ch>
2022-08-19 10:54:50 +0200

    Basic sidebar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Page from './Page';
+import * as resume from './resume.json'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Page />
+    <Page {...resume}/>
   </React.StrictMode>
 );
